23de98aa6b0c5a3b16a3d9e739e88ce29a742008
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-29 04:47:37 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -42,16 +42,16 @@ end
 
 # Refactor the above code using attribute accessors and attribute readers!
 
-class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+# class Book
+#   attr_accessor :author, :page_count, :genre
+#   attr_reader :title
   
-  def initialize(title)
-    @title = title
-  end
+#   def initialize(title)
+#     @title = title
+#   end
   
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-end
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+# end
 
